8c52886a851b301d2594eb5162a93faf780f8024
Meagan Campbell <meagancampbell@users.noreply.github.com>
2021-06-17 20:26:48 +0000

    MC

--- index.js
+++ index.js
@@ -3,23 +3,23 @@
 
 // 1. Declare and assign the variables here:
 let shuttleName = 'Determination';
-let shuttleSpeedMph = '17500';
-let distanceToMarsKm = '225000000';
-const milesPerKm = '0.621';
-
+let shuttleSpeedMph = 17500;
+let distanceToMarsKm = 225000000;
+let distanceToMoonKm = 384400;
+const milesPerKm = 0.621;
 let milesToMars = distanceToMarsKm * milesPerKm;
 let hoursToMars = milesToMars / shuttleSpeedMph;
 let daysToMars = hoursToMars / 24;
 
 // 2. Use console.log to print the 'typeof' each variable. Print one item per line.
 console.log(typeof 'shuttleName');
-console.log(typeof 'shuttleSpeedMph');
-console.log(typeof 'distanceToMarsKm');
-console.log(typeof 'distanceToMoonKm');
-console.log(typeof 'milesPerKm');
-console.log(typeof 'milesToMars');
-console.log(typeof 'hoursToMars');
-console.log(typeof 'daysToMars');
+console.log(typeof shuttleSpeedMph);
+console.log(typeof distanceToMarsKm);
+console.log(typeof distanceToMoonKm);
+console.log(typeof milesPerKm);
+console.log(typeof milesToMars);
+console.log(typeof hoursToMars);
+console.log(typeof daysToMars);
 
 console.log(shuttleName + " will take " + daysToMars + " days to reach Mars.")
 
